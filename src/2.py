76eb2c4e1b8e58710c0e36fed6306dd5e9660fb6
fiorentinogiuseppe <fiorentinogiuseppebcc@gmail.com>
2020-02-05 17:44:10 -0300

    Renomeei os dados, fiz as questoes 2 e criei o docker do postgresql

---
+++ src/2.py
@@ -0,0 +1,45 @@
+from collections import Counter
+from pymongo import MongoClient
+
+client = MongoClient('localhost',
+                     port=27017,
+                     username='root',
+                     password='Testeintelivix2020!',
+                     authSource='admin')
+
+db = client['intelivix']
+
+my_collection = db['my_collection']
+
+
+total_andamentos = 0
+for item in my_collection.find():
+    total_andamentos += len(item['andamentos'])
+
+
+print("Contagem total dos processos: ",my_collection.count_documents({}))
+
+print("Contagem total dos andamentos: ",total_andamentos)
+
+print("\nContagem de processos por estado")
+
+count_by_states = my_collection.aggregate([
+    # Now lets group on the name counting how many grouped documents we have
+    {"$group":{"_id":"$estado", "sum":{"$sum":1}}},
+    # Sort ascending.
+    {"$sort": {"_id": 1}}
+])
+for states_count in count_by_states:
+    print("O estado de {} tem um total de {} processos".format(states_count['_id'], states_count['sum']))
+
+# for case insensitive search options i
+
+print("\nContagem de juízes que começam com 'S': ", my_collection.count_documents({"juiz": {'$regex': "^S", "$options" : "i"}}))
+
+print("Contagem de etiquetas mais comuns: ")
+list_etiquetas = [i['andamentos'][0]['etiquetas'] for i in my_collection.find({},{'andamentos.etiquetas' :1})]
+Output = Counter([tuple(i) for i in list_etiquetas])
+for key, value in Output.items():
+    print("A Etiqueta {} teve {} ocorrencias".format(key, value))
+
+client.close()
--- test/download_datasets.py
+++
@@ -1,9 +0,0 @@
-import wget
-
-with open('../datasets/datasets.txt', 'r') as f:
-    datasets = f.readlines()
-
-for dataset in datasets:
-    url = dataset.split('\n')[0]
-    out_path = '../datasets/' + url.rsplit("/",1)[1]
-    filename = wget.download(url, out=out_path)
